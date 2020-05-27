7bf762bba69356bb75cb4d791e88c447c8c993a9
rumigor <rumigor@gmail.com>
2020-05-27 13:09:18 +0300

    Задание выполнено

--- src/hw5/Person.java
+++ src/hw5/Person.java
@@ -17,7 +17,14 @@ public class Person {
         this.age = age;
     }
 
-    public int getAge() {
-        return age;
+    public static void printInfo(Person [] person){
+        System.out.printf("%-30s%-20s%-20s%-15s%-14s%-7s%n", "ФИО", "Должность", "E-mail", "Телефон", "Зарплата (руб)", "возраст");
+        for (int i = 0; i < person.length; i++) {
+            if (person[i].age > 40) {
+                System.out.printf("%-30s%-20s%-20s%-15s%-14d%-7d%n", person[i].name, person[i].position, person[i].email,
+                        person[i].tel, person[i].salary, person[i].age);
+            }
+        }
+
     }
 }
